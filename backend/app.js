e26dbd22bb68a050cf59d76eb762dc206a861881
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2022-10-04 23:36:41 +0530

    created base routes

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app = express();
+
+const errorMiddleware = require("./middleware/error")
+app.use(express.json());
+//Route imports
+const product = require('./routes/productRoute');
+
+app.use("/api/v1",product);
+
+//Middleware for error
+app.use(errorMiddleware);
+
+module.exports = app; // Modular programming so that other modules can access it.
