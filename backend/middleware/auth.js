47ae08cebb0b297cb349405bdaaac69cce057cb8
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2022-10-25 22:10:38 +0530

    updated product routes for authenticated users only

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,24 +1,33 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
-const jwt=require('jsonwebtoken');
-const User = require('../models/userModel')
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
-exports.isAuthenticatedUser = catchAsyncErrors(async(req,res,next)=>{
-    const {token} = req.cookies;
-    // console.log(token);
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+  // console.log(token);
 
-    if(!token){
-        return next(new ErrorHandler("PLease login to access this resource",401));
-    }
+  if (!token) {
+    return next(new ErrorHandler("PLease login to access this resource", 401));
+  }
 
-    const decodedData = jwt.verify(token,process.env.JWT_SECRET);
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = await User.findById(decodedData.id);
-    next(); 
+  req.user = await User.findById(decodedData.id);
+  next();
 });
 
-exports.authorizedRoles=(...roles)=>{
-    return (req,res,next)=>{
-        // if(roles.includes())
+exports.authorizedRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      // Here our roles will contain "admin" so if user with role as admin comes then only he will be allowed to get the result.
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resource`,
+          403
+        )
+      );
     }
-}
+    next();
+  };
+};
